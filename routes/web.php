bb7bf69b8e98fa9232b46aace8fa0dbd695fd9e8
MariamaBalde <foulah18@gmail.com>
2025-10-23 18:27:55 +0000

    swagger locale terminate

--- routes/web.php
+++ routes/web.php
@@ -18,6 +18,13 @@ Route::get('/', function () {
 });
 
 Route::get('/api/documentation', function () {
-return view('l5-swagger::index', ['documentation' => 'default']);
+    $documentation = 'default';
+    $urlToDocs = route('l5-swagger.'.$documentation.'.docs');
+    $configUrl = config('l5-swagger.defaults.additional_config_url');
+    $validatorUrl = config('l5-swagger.defaults.validator_url');
+    $operationsSorter = config('l5-swagger.defaults.operations_sort');
+    $useAbsolutePath = config('l5-swagger.documentations.'.$documentation.'.paths.use_absolute_path', false);
+
+    return view('l5-swagger::index', compact('documentation', 'urlToDocs', 'configUrl', 'validatorUrl', 'operationsSorter', 'useAbsolutePath'));
 });
 
