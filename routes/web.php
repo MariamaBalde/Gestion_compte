1eaf84baabc988ba3512c31ffe33ae12ea11e348
MariamaBalde <foulah18@gmail.com>
2025-10-24 13:44:38 +0000

    debug swagger2

--- routes/web.php
+++ routes/web.php
@@ -29,3 +29,64 @@ Route::get('/api/docs', function () {
     return view('swagger');
 });
 
+Route::get('/api-docs.json', function () {
+    return response()->json([
+        'openapi' => '3.0.0',
+        'info' => [
+            'title' => 'API de gestion de comptes',
+            'version' => '1.0.0',
+            'description' => 'Documentation de l\'API bancaire'
+        ],
+        'servers' => [
+            [
+                'url' => 'https://gestion-compte-1izl.onrender.com/api/v1',
+                'description' => 'Serveur de production'
+            ]
+        ],
+        'paths' => [
+            '/comptes' => [
+                'get' => [
+                    'summary' => 'Lister les comptes',
+                    'parameters' => [
+                        [
+                            'name' => 'page',
+                            'in' => 'query',
+                            'schema' => ['type' => 'integer'],
+                            'description' => 'Numéro de page'
+                        ],
+                        [
+                            'name' => 'limit',
+                            'in' => 'query',
+                            'schema' => ['type' => 'integer'],
+                            'description' => 'Nombre d\'éléments par page'
+                        ],
+                        [
+                            'name' => 'type',
+                            'in' => 'query',
+                            'schema' => ['type' => 'string'],
+                            'description' => 'Filtrer par type de compte'
+                        ],
+                        [
+                            'name' => 'statut',
+                            'in' => 'query',
+                            'schema' => ['type' => 'string'],
+                            'description' => 'Filtrer par statut'
+                        ],
+                        [
+                            'name' => 'search',
+                            'in' => 'query',
+                            'schema' => ['type' => 'string'],
+                            'description' => 'Recherche par numéro ou titulaire'
+                        ]
+                    ],
+                    'responses' => [
+                        '200' => [
+                            'description' => 'Liste des comptes récupérée avec succès'
+                        ]
+                    ]
+                ]
+            ]
+        ]
+    ]);
+});
+
